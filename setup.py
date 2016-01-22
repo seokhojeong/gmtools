f2615de3b9c09e0ffe143511d29bb5d05c2ff9c1
Seokho Jeong <seokho.jeong@canterbury.ac.nz>
2016-01-22 14:34:12 +1300

    modified setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("gmtools_c.pyx")
+    ext_modules = cythonize("gmtools_c.pyx"),
+    include_dirs = [numpy.get_include()]
 )
