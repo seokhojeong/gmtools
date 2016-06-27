3da35fbf87e65e7d0f6c4e6cbf2417b100ff8d74
Seokho Jeong <sje96@enci-sje96.local>
2016-06-28 07:33:37 +1200

    Updated 'setup.py'

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize("gmtools_c.pyx"),
+    ext_modules = cythonize("RSpectra.pyx"),
     include_dirs = [numpy.get_include()]
 )
